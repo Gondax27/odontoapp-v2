1eaea86d7f5200b71ceb8d21dee995a993e14add
Juan Jose Gonzalez <juanjogonda@gmail.com>
2024-05-23 19:14:42 -0500

    Fix in sidebar component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -17,19 +17,22 @@ const SidebarElement = ({ allowOutsideClick, element, show, setShow }: SidebarPr
     else document.body.style.overflow = 'auto';
   }, [show]);
 
-  const handleClose = useCallback(() => setShow(!show), [show, setShow]);
+  const handleClose = useCallback((ev: React.MouseEvent) => {
+    ev.stopPropagation();
+    setShow(!show)
+  }, [show, setShow]);
 
   return (
     show && (
-      <div className='min-w-full min-h-dvh'>
+      <div className='absolute inset-0'>
         <div
-          className={`absolute inset-0 z-[2] bg-black/30 select-none ${
+          className={`absolute inset-0 z-[2] bg-black/30 ${
             !allowOutsideClick ? 'pointer-events-none' : ''
           }`}
           onClick={handleClose}
         />
 
-        <aside className='animation-sidebar-in absolute inset-y-0 right-0 w-[32rem] z-[3] bg-white p-8'>
+        <aside className='animation-sidebar-in absolute inset-y-0 right-0 w-[32rem] z-[3] bg-white p-8 !overflow-y-auto h-dvh'>
           <header className='flex items-start justify-end w-full'>
             <button
               className='p-2 text-gray-500 hover:text-purple-900 transition-[color] w-fit'
